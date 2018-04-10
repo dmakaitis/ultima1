415d22c9d9e61338add45f4b0207efdd17c550c4
Darius Makaitis <darius@portkullis.com>
2018-04-10 06:52:45 -0500

    Updated bitmap viewer to be able to view compressed images again

--- assets/readme.txt
+++ assets/readme.txt
@@ -3,15 +3,17 @@ The following asset files are required by the build:
 File 				Used by 	Description
 
 intro/horse0.bin	IN.PRG		Animation frames for the horse on the intro page. Each frame is
-intro/horse1.bin	IN.PRG		stored as the first two columns of sprite data (the third is
-intro/horse2.bin	IN.PRG		set to zeros by the intro code).
-intro/horse3.bin	IN.PRG
+intro/horse1.bin	IN.PRG		stored as a 24x14 bitmap stored in row sequential order (the
+intro/horse2.bin	IN.PRG		remaining bytes of the C64 sprite are set to zeros by the intro 
+intro/horse3.bin	IN.PRG		code).
 intro/horse4.bin	IN.PRG
 intro/horse5.bin	IN.PRG
 intro/horse6.bin	IN.PRG
 
-intro/image.bin 	IN.PRG		Compressed image to display on the intro page when loading the game.
+intro/image.bin 	IN.PRG		Compressed 320x200 image to display on the intro page when loading the game.
 
 intro/osi.bin		IN.PRG		104x21 Origin systems logo, arranged top row of pixels, then second, etc.
 
-intro/ultima.bin	IN.PRG		Ultima I logo, arranged in C64 bitmap format (first 8x8 block, then second 8x8, etc.)
+intro/ultima.bin	IN.PRG		184x48 Ultima I logo, arranged in C64 bitmap format (first 8x8 block, then second 8x8, etc.)
+
+lo/font.bin			LO.PRG		The game font, stored as a 128x64 bitmap arranged in C64 bitmap format (8x8 blocks)
--- util/viewbitmap.cpp
+++ util/viewbitmap.cpp
@@ -11,56 +11,124 @@ enum ArgState {
 	asDone
 };
 
+/*
+ * Filters an input stream, optionally reading it as a RLE compressed
+ * stream. The RLE works by reading the stream normally, but the
+ * following byte values are run-length encoded, with the following
+ * byte being the number of repeats (with '0x00' being read as 256):
+ *
+ * 		0x00, 0x10, 0x16, 0x50, 0xe6, 0xff
+ *
+ * In additionl the value 0xda is treated as an end-of-file marker.
+ */
+class StreamFilter {
+
+private:
+	std::ifstream input;
+	bool compressed;
+
+	int byte;
+	int repeats;
+	bool endOfCompressed;
+
+public:
+	StreamFilter(char *filename, bool compressed) : 
+		input(filename, std::ios::binary), 
+		compressed(compressed),
+		repeats(0),
+		endOfCompressed(false)
+	{}
+
+	int get() {
+		while(repeats == 0) {
+			byte = input.get();
+
+			if(compressed) {
+				switch(byte) {
+					case 0xda:
+						endOfCompressed = true;
+						break;
+					case 0x00:
+					case 0x10:
+					case 0xff:
+					case 0xe6:
+					case 0x16:
+					case 0x50:
+						repeats = input.get();
+						// Treat '0' repeats as 256...
+						if(repeats == 0) repeats = 256;
+						break;
+					default:
+						repeats = 1;
+				}
+			} else {
+				repeats = 1;
+			}
+		}
+
+		--repeats;
+		return byte;
+	}
+
+	bool eof() {
+		return endOfCompressed || input.eof();
+	}
+
+};
+
+/*
+ * Displays a bitmap image as text on the screen. The bitmap may be read in either
+ * row sequential format, or in 8x8 block format (like a C64 bitmap). In either
+ * case, the image width must be given and must be a multiple of 8.
+ *
+ * 	filename		the name of the file containing the bitmap
+ *  imageWidth		the widge of the bitmap in pixels
+ *  blockFormat 	true if the image is stored as 8x8 blocks; false if the image is row sequential
+ *  compressed      true if the image is RLE compressed; false otherwise
+ */
 void displayBitmap(char* filename, int imageWidth, bool blockFormat, bool compressed) {
-	std::ifstream input(filename, std::ios::binary);
+	if(imageWidth % 8 != 0) {
+		std::cout << "ERROR: Image width must be a multiple of 8" << std::endl;
+		return;
+	}
+
+	StreamFilter input(filename, compressed);
 
 	if(blockFormat) {
-		if(compressed) {
-			std::cout << "filename: " << filename << std::endl;
-			std::cout << "width: " << imageWidth << std::endl;
-			std::cout << "block: " << blockFormat << std::endl;
-			std::cout << "compressed: " << compressed << std::endl;
-		} else {
-			if(imageWidth % 8 != 0) {
-				std::cout << "ERROR: Image width must be a multiple of 8" << std::endl;
-				return;
-			}
+		int blocksPerRow = imageWidth / 8;
+		std::unique_ptr<char[]> buffer(new char[imageWidth]);
 
-			int blocksPerRow = imageWidth / 8;
-			std::unique_ptr<char[]> buffer(new char[imageWidth]);
+		int colPtr = 0;
+		int ptr = 0;
+		int count = 0;
 
-			int colPtr = 0;
-			int ptr = 0;
-			int count = 0;
+		int byte = input.get();
+		while(!input.eof()) {
+			buffer[ptr] = byte;
 
-			int byte = input.get();
-			while(!input.eof()) {
-				buffer[ptr] = byte;
-
-				count++;
-				if(count % imageWidth == 0) {
-					// Print 8 rows...
-					ptr = 0;
-					for(int row = 0; row < 8; row++) {
-						for(int col = 0; col < blocksPerRow; col++) {
-							printAsBinary(buffer[ptr++]);
-						}
-						std::cout << std::endl;
+			count++;
+			if(count % imageWidth == 0) {
+				// Print 8 rows...
+				ptr = 0;
+				for(int row = 0; row < 8; row++) {
+					for(int col = 0; col < blocksPerRow; col++) {
+						printAsBinary(buffer[ptr++]);
 					}
-					
-					count = 0;
-					colPtr = 0;
-					ptr = colPtr;
-				} else if(count % 8 == 0) {
-					colPtr++;
-					ptr = colPtr;
-				} else {
-					ptr += blocksPerRow;
+					std::cout << std::endl;
 				}
-				byte = input.get();
+				
+				count = 0;
+				colPtr = 0;
+				ptr = colPtr;
+			} else if(count % 8 == 0) {
+				colPtr++;
+				ptr = colPtr;
+			} else {
+				ptr += blocksPerRow;
 			}
-			std::cout << std::endl;
+			byte = input.get();
 		}
+		std::cout << std::endl;
 	} else {
 		int x = 0;
 		int byte = input.get();
@@ -79,6 +147,9 @@ void displayBitmap(char* filename, int imageWidth, bool blockFormat, bool compre
 	}
 }
 
+/*
+ * Parses arguments, then passes control to displayBitmap
+ */
 int main(int argc, char** argv) {
 	int width = -1;
 	char* filename = NULL;
@@ -122,61 +193,15 @@ int main(int argc, char** argv) {
 	}
 
 	if(help || !filename || (width <= 0)) {
-		std::cout << "Usage: " << argv[0] << " [-b] [filename] [width]" << std::endl;
+		std::cout << "Usage: " << argv[0] << " [OPTIONS] FILENAME WIDTH" << std::endl;
+		std::cout << std::endl;
+		std::cout << "Options:" <<std::endl;
+		std::cout << "  -b      The image is stored as 8x8 blocks of pixels" << std::endl;
+		std::cout << "  -c      The image is compressed using RLE" << std::endl;
 		exit(1);
 	}
 
 	displayBitmap(filename, width, block, compressed);
-
-	exit(0);
-
-	char buffer[8 * 40 * 25];
-
-	for(int i = 0; i < 8 * 40 * 25; i++) {
-		buffer[i] = 0x00;
-	}
-
-	int ptr = 0;
-	bool done = false;
-
-	std::ifstream input(argv[1], std::ios::binary);
-	while(!input.eof() && !done) {
-		int code = input.get();
-		int reps = 1;
-
-		switch(code) {
-			case 0xda:
-				done = true;
-				break;
-			case 0x00:
-			case 0x10:
-			case 0xff:
-			case 0xe6:
-			case 0x16:
-			case 0x50:
-				reps = input.get();
-			default:
-				for(int i = 0; i < reps; i++) {
-					buffer[ptr++] = code;
-				}
-		}
-	}
-
-	ptr = 0;
-
-	for(int y = 0; y < 25; y++) {
-		int rowPtr = ptr;
-		for(int yy = 0; yy < 8; yy++) {
-			int bytePtr = rowPtr;
-			for(int x = 0; x < 40; x++) {
-				printAsBinary(buffer[bytePtr]);
-				bytePtr += 8;
-			}
-			std::cout << std::endl;
-			rowPtr++;
-		}
-		ptr += 8 * 40;
-	}
 
 	return 0;
 }
