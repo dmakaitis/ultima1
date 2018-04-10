60b8a3a338f500e88fbccf10a7a4fa5b188ce313
Darius Makaitis <darius@portkullis.com>
2018-04-10 16:14:21 -0500

    Tweaked argument parsing code

--- util/viewbitmap.cpp
+++ util/viewbitmap.cpp
@@ -4,10 +4,14 @@
 #include <iostream>
 #include <vector>
 
+#include <getopt.h>
+#include <png.h>
+
 #include "bitmaputils.hpp"
 
 enum ArgState {
 	asFilename,
+	asOutFilename,
 	asWidth,
 	asDone
 };
@@ -101,9 +105,10 @@ std::vector<char> loadBitmap(char* filename, int imageWidth, bool blockFormat, b
 		std::unique_ptr<char[]> buffer(new char[imageWidth]);
 
 		int bytesPerRow = imageWidth / 8;
+
+		int count = 0;
 		int colPtr = 0;
 		int ptr = 0;
-		int count = 0;
 
 		int byte = input.get();
 		while(!input.eof()) {
@@ -145,51 +150,57 @@ std::vector<char> loadBitmap(char* filename, int imageWidth, bool blockFormat, b
 int main(int argc, char** argv) {
 	int width = -1;
 	char* filename = NULL;
+	char* outFilename = NULL;
 	ArgState argState = asFilename;
+	ArgState argStateBuffer = asFilename;
 	bool help = false;
 	bool block = false;
 	bool compressed = false;
 
-	for(int i = 1; i < argc; i++) {
-		char* arg = argv[i];
-		if(*arg == '-') {
-			for(int j = 1; arg[j]; j++) {
-				switch(arg[j]) {
-					case '?':
-						help = true;
-						break;
-					case 'b':
-						block = true;
-						break;
-					case 'c':
-						compressed = true;
-						break;
-				}
-			}
-		} else {
-			switch(argState) {
-				case asFilename:
-					filename = argv[i];
-					argState = asWidth;
-					break;
-				case asWidth:
-					width = atoi(argv[i]);
-					argState = asDone;
-					break;
-				default:
-					help = true;
-					argState = asDone;
-					break;
-			}
+	static struct option options[] = {
+		{ "help",		no_argument,		0,	'?' },
+		{ "block",		no_argument,		0,	'b' },
+		{ "compressed",	no_argument,		0,	'c' },
+		{ "input",		required_argument,	0,	'i' },
+		{ "output",		required_argument,	0,	'o' },
+		{ "width",		required_argument,	0,	'w' },
+	};
+
+	int option_index = 0;
+
+	int c;
+	while((c = getopt_long(argc, argv, "bci:o:w:", options, &option_index)) != -1) {
+		switch(c) {
+			case 'b':
+				block = true;
+				break;
+			case 'c':
+				compressed = true;
+				break;
+			case 'i':
+				filename = optarg;
+				break;
+			case 'o':
+				outFilename = optarg;
+				break;
+			case 'w':
+				width = atoi(optarg);
+				break;
+			case '?':
+				help = true;
+				break;
 		}
 	}
 
 	if(help || !filename || (width <= 0)) {
-		std::cout << "Usage: " << argv[0] << " [OPTIONS] FILENAME WIDTH" << std::endl;
+		std::cout << "Usage: " << argv[0] << " [OPTIONS]" << std::endl;
 		std::cout << std::endl;
 		std::cout << "Options:" <<std::endl;
 		std::cout << "  -b      The image is stored as 8x8 blocks of pixels" << std::endl;
 		std::cout << "  -c      The image is compressed using RLE" << std::endl;
+		std::cout << "  -i      Set the input filename (required)" << std::endl;
+		std::cout << "  -o      Set the output filename to save in PNG format" << std::endl;
+		std::cout << "  -w      Set the image width in pixels (required)" << std::endl;
 		exit(1);
 	}
 
@@ -209,5 +220,41 @@ int main(int argc, char** argv) {
 
 	std::cout << std::endl;
 
+	int height = bitmap.size() / bytesPerRow;
+	int bit_depth = 1;
+
+	std::cout << "Image size: " << width << " x " << bitmap.size() / bytesPerRow << std::endl;
+
+	// Save it as a PNG:
+	if(outFilename) {
+		// Step 4.1 - Setup
+
+		FILE* fp = fopen(outFilename, "wb");
+		png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+		png_infop info_ptr = png_create_info_struct(png_ptr);
+		// setup error callbacks
+		png_init_io(png_ptr, fp);
+
+		// Step 4.2 - Write callbacks
+
+		// Step 4.3 - Info contents
+
+		png_set_IHDR(png_ptr, info_ptr, width, height, bit_depth, PNG_COLOR_TYPE_GRAY, 
+			PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
+		// Step 4.5 - Write file
+
+		std::vector<png_bytep> row_pointers;
+		png_bytep bitmapPtr = (png_bytep)(&(*(bitmap.begin())));
+		for(int i = 0; i < height; i++) {
+			row_pointers.push_back(bitmapPtr);
+			bitmapPtr += bytesPerRow;
+		}
+		png_set_rows(png_ptr, info_ptr, &(*(row_pointers.begin())));
+
+		png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+
+		fclose(fp);
+	}
 	return 0;
 }
