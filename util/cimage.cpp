13ea931f0d4281d1046e214d762761f3a22d6101
Darius Makaitis <darius@portkullis.com>
2018-04-14 08:31:39 -0500

    Refactored to make cimage code easier to work with

--- util/cimage.cpp
+++ util/cimage.cpp
@@ -3,16 +3,69 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <cstdint>
 
 #include <getopt.h>
 #include <png.h>
 
-void printAsBinary(char c) {
+typedef std::vector<uint8_t> ByteArray;
+
+void printAsBinary(uint8_t c) {
     int mask = 0x80;
     while(mask) {
         std::cout << (c & mask ? "*" : ".");
         mask = mask >> 1;
     }
+}
+
+void printBitmap(ByteArray& bitmap, int width, int height) {
+    auto ptr = bitmap.begin();
+
+    for(int y = 0; y < height; y++) {
+        for(int x = 0; x < width; x += 8) {
+            printAsBinary(*ptr++);
+        }
+        std::cout << std::endl;
+    }
+}
+
+ByteArray convertToBitmap(png_structp png_ptr, png_infop info_ptr) {
+    ByteArray bitmap;
+
+    int width = png_get_image_width(png_ptr, info_ptr);
+    int height = png_get_image_height(png_ptr, info_ptr);
+    png_bytepp row_pointers = png_get_rows(png_ptr, info_ptr);
+
+    for(int y = 0; y < height; y++) {
+        png_bytep ptr = row_pointers[y];
+        for(int x = 0; x < width; x += 8) {
+            bitmap.push_back(*ptr++);
+        }
+    }
+
+    return bitmap;
+}
+
+ByteArray convertToBlockFormat(ByteArray bitmap, int width, int height) {
+    ByteArray buffer;
+
+    if(height % 8 != 0) {
+        std::cerr << "Image height must be a multiple of 8 to save in block format" << std::endl;
+        exit(1);
+    }
+
+    int bytesPerRow = width / 8;
+
+    for(int y = 0; y < height; y += 8) {
+        int x, xptr;
+        for(x = 0, xptr = 0; x < width; x += 8, xptr++) {
+            for(int yy = 0; yy < 8; yy++) {
+                buffer.push_back(bitmap[(y + yy) * bytesPerRow + xptr]);
+            }
+        }
+    }
+
+    return buffer;
 }
 
 int readExtraData(png_structp png_ptr, png_unknown_chunkp chunk) {
@@ -28,6 +81,84 @@ int readExtraData(png_structp png_ptr, png_unknown_chunkp chunk) {
     } else {
         return 0;
     }
+}
+
+void saveFile(const char* filename, ByteArray& buffer, bool compressed) {
+    std::ofstream output(filename, std::ios::binary);
+
+    if(compressed) {
+        for(auto b = buffer.begin(); b != buffer.end(); b++) {
+            uint8_t value = *b;
+
+            output.put(value);
+
+            switch(value) {
+                case 0x00:
+                case 0x10:
+                case 0xff:
+                case 0xe6:
+                case 0x16:
+                case 0x50:
+                    int repeats = 0;
+                    while(*b == value && b != buffer.end() && repeats < 256) {
+                        repeats++;
+                        b++;
+                    }
+                    b--;
+                    if(repeats == 256) {
+                        repeats = 0;
+                    }
+                    output.put((uint8_t) repeats);
+            }
+        }
+        // Write end of compressed image marker:
+        output.put(0xda);
+    } else {
+        output.write((const char*) &buffer[0], buffer.size());
+    }
+}
+
+ByteArray loadPng(const char* filename, int &width, int &height, ByteArray& extraData) {
+        
+    // Step 3.1 - Setup
+
+    FILE* fp = fopen(filename, "rb");
+    if(!fp) {
+        std::cerr << "Failed to open input file: " << filename << std::endl;
+        exit(1);
+    }
+
+    // fread(header, 1, number, fp);
+    // is_png = !png_sig_cmp(header, 0, number);
+    // if(!is_png) {
+    //     std::cerr << "Input file is not a PNG image" << std::endl;
+    //     exit(1);
+    // }
+
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if(!png_ptr) {
+        std::cerr << "Failed to create read PNG struct" << std::endl;
+        exit(1);
+    }
+
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if(!info_ptr) {
+        std::cerr << "Failed to create PNG info struct" << std::endl;
+        exit(1);
+    }
+
+    png_set_read_user_chunk_fn(png_ptr, &extraData, readExtraData);
+
+    png_init_io(png_ptr, fp);
+    png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+
+    width = png_get_image_width(png_ptr, info_ptr);
+    height = png_get_image_height(png_ptr, info_ptr);
+
+    std::cout << "Image size: " << width << " x " << height << std::endl;
+    std::cout << "Extra data: " << extraData.size() << " bytes" << std::endl;
+
+    return convertToBitmap(png_ptr, info_ptr);
 }
 
 /*
@@ -90,131 +221,21 @@ int main(int argc, char** argv) {
 
     // Read the PNG image:
         
-    // Step 3.1 - Setup
-
-    FILE* fp = fopen(filename, "rb");
-    if(!fp) {
-        std::cerr << "Failed to open input file: " << filename << std::endl;
-        exit(1);
-    }
-
-    // fread(header, 1, number, fp);
-    // is_png = !png_sig_cmp(header, 0, number);
-    // if(!is_png) {
-    //     std::cerr << "Input file is not a PNG image" << std::endl;
-    //     exit(1);
-    // }
-
-    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if(!png_ptr) {
-        std::cerr << "Failed to create read PNG struct" << std::endl;
-        exit(1);
-    }
-
-    png_infop info_ptr = png_create_info_struct(png_ptr);
-    if(!info_ptr) {
-        std::cerr << "Failed to create PNG info struct" << std::endl;
-        exit(1);
-    }
-
-    // png_set_keep_unknown_chunks(png_ptr, PNG_HANDLE_CHUNK_ALWAYS, (const unsigned char *)"daTa", 1);
-    // png_set_keep_unknown_chunks(png_ptr, PNG_HANDLE_CHUNK_ALWAYS, NULL, 0);
-    std::vector<char> extraData;
-    png_set_read_user_chunk_fn(png_ptr, &extraData, readExtraData);
-
-    png_init_io(png_ptr, fp);
-    png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
-
-    int width = png_get_image_width(png_ptr, info_ptr);
-    int height = png_get_image_height(png_ptr, info_ptr);
-
-    std::cout << "Image size: " << width << " x " << height << std::endl;
-    std::cout << "Extra data: " << extraData.size() << " bytes" << std::endl;
-
-    // png_unknown_chunkpp unknownChunks;
-    // int unknownChunkCount = png_get_unknown_chunks(png_ptr, info_ptr, unknownChunks);
-    // if(unknownChunkCount > 0) {
-    //     std::cout << "Found " << unknownChunkCount << " unknown chunks";
-    // }
-
-    png_bytepp row_pointers = png_get_rows(png_ptr, info_ptr);
+    int width, height;
+    ByteArray extraData;
+    ByteArray bitmap = loadPng(filename, width, height, extraData);
 
     if(!quiet) {
-        for(int y = 0; y < height; y++) {
-            png_bytep ptr = row_pointers[y];
-            // std::cout << "Got pointer for row " << y << ": " << (long) ptr << std::endl;
-            for(int x = 0; x < width; x += 8) {
-                printAsBinary(*ptr++);
-            }
-            std::cout << std::endl;
-        }
+        printBitmap(bitmap, width, height);
     }
 
     if(outFilename) {
-
-        // Copy data to buffer
-
-        std::vector<char> buffer;
-        if(block) {
-            if(height % 8 != 0) {
-                std::cerr << "Image height must be a multiple of 8 to save in block format" << std::endl;
-                exit(1);
-            }
-
-            for(int y = 0; y < height; y += 8) {
-                int x, xptr;
-                for(x = 0, xptr = 0; x < width; x += 8, xptr++) {
-                    for(int yy = 0; yy < 8; yy++) {
-                        buffer.push_back(row_pointers[y + yy][xptr]);
-                    }
-                }
-            }
-        } else {
-            for(int y = 0; y < height; y++) {
-                png_bytep ptr = row_pointers[y];
-                for(int x = 0; x < width; x += 8) {
-                    buffer.push_back(*ptr++);
-                }
-            }
-        }
+        ByteArray buffer = block ? convertToBlockFormat(bitmap, width, height) : bitmap;
 
         // Append any extra data...
         buffer.insert(buffer.end(), extraData.begin(), extraData.end());
 
-        // Now write the buffer to the file
-
-        std::ofstream output(outFilename, std::ios::binary);
-
-        if(compressed) {
-            for(std::vector<char>::const_iterator b = buffer.begin(); b != buffer.end(); b++) {
-                char value = *b;
-
-                output.put(value);
-
-                switch((unsigned char) value) {
-                    case 0x00:
-                    case 0x10:
-                    case 0xff:
-                    case 0xe6:
-                    case 0x16:
-                    case 0x50:
-                        int repeats = 0;
-                        while(*b == value && b != buffer.end() && repeats < 256) {
-                            repeats++;
-                            b++;
-                        }
-                        b--;
-                        if(repeats == 256) {
-                            repeats = 0;
-                        }
-                        output.put((unsigned char) repeats);
-                }
-            }
-            // Write end of compressed image marker:
-            output.put(0xda);
-        } else {
-            output.write(&buffer[0], buffer.size());
-        }
+        saveFile(outFilename, buffer, compressed);
     }
 
     return 0;
